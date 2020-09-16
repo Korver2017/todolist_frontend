3521dd7cd851ec65154107f594ee92b1d3c94dd8
Korver Huang <korver2017@gmail.com>
2020-09-16 13:04:04 +0800

    Binding up destroy function

--- js/index.js
+++ js/index.js
@@ -1,42 +1,43 @@
 $(document).ready (function () {
 
+  let $todolist;
+
   let $retrieve = () => {
     axios.get ('http://localhost:3000/')
     .then (res => {
-      console.log (res.data);
 
-      let data = res.data;
+      $todolist = res.data.map (d => {
+        d.checked = false;
+        return d;
+      });
+
+      console.log ($todolist);
       
       let $inputGroup = 
         `<div class="input-group mt-3">
           <div class="input-group-prepend">
             <div class="input-group-text">
-              <input type="checkbox" aria-label="Checkbox for following text input">
+              <input class="checkbox" type="checkbox" aria-label="Checkbox for following text input">
             </div>
           </div>
-            <input value="" readonly type="text" class="form-control">
-        </div>`
-
-      console.log ($inputGroup);
+          <input value="" readonly type="text" class="form-control">
+        </div>`;
 
       $('.todo-list').text (' ');
 
-      data.forEach ((d, i) => {
-        console.log (d);
-        $('.todo-list').append ($inputGroup);
-        // console.log ($($('.todo-list').find ('.form-control')[1]));
-        $($('.todo-list').find ('.form-control')[i]).attr ('value', d.todo_id + ', ' + d.todo_title + ', ' + d.todo_desc);
-      })
+      $todolist.forEach ((todo, i) => {
 
-      // $('ul').append ('<li>K!</li>').append ('<li>K!</li>').append ('<li>K!</li>')
-    })
+        $('.todo-list').append ($inputGroup);
+        $($('.todo-list').find ('.form-control')[i]).attr ('value', todo.todo_id + ', ' + todo.todo_title + ', ' + todo.todo_desc);
+      });
+    });
   }
 
   $retrieve ();
   
   $('.create-button').click (function () {
 
-    axios.post ('http://localhost:3000/add', {todo_title: 'PHP', todo_desc: 'Learn PHP', submission_date: '2020-09-14'})
+    axios.post ('http://localhost:3000/add', {todo_title: 'SCSS', todo_desc: 'Learn SCSS', submission_date: '2020-09-14'})
       .then (res => {
         console.log (res.data);
 
@@ -46,7 +47,7 @@ $(document).ready (function () {
 
   $('.update-button').click (function () {
 
-    axios.put('http://localhost:3000/update', {todo_id: 16, todo_title: 'CSS', todo_desc: 'Learn CSS', submission_date: '2020-09-16'})
+    axios.put ('http://localhost:3000/update', {todo_id: 16, todo_title: 'CSS', todo_desc: 'Learn CSS', submission_date: '2020-09-16'})
       .then (res => {
         console.log (res.data);
 
@@ -56,12 +57,35 @@ $(document).ready (function () {
 
   $('.delete-button').click (function () {
 
-    axios.post ('http://localhost:3000/remove', {todo_id: 14})
-      .then (res => {
-        console.log (res.data);
+    $('.checkbox').each (function (i) {
+      
+      if ($(this).is (':checked')) {
+        console.log (i);
+        $todolist[i].checked = true;
+      }
 
-        $retrieve ();
-      })
+      else {
+        $todolist[i].checked = false;
+      }
+    });
+
+    console.log ($todolist);
+
+    let $destroyLoader = $todolist.map (todo => {
+
+        if (todo.checked) {
+
+        axios.post ('http://localhost:3000/remove', {todo_id: todo.todo_id})
+        .then (res => {
+
+          console.log (res.data);
+
+          $retrieve ();
+        })
+      }
+    });
+
+    Promise.all ($destroyLoader);
   });
 
 });
