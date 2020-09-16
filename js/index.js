16d148034de2a4a66c5e58332592624cdd3e1a74
Korver Huang <korver2017@gmail.com>
2020-09-16 08:11:39 +0800

    Add style & set retrieve function

--- js/index.js
+++ js/index.js
@@ -1,28 +1,67 @@
-axios.get ('http://localhost:3000/')
-  .then (res => {
-    console.log (res.data);
-  })
+$(document).ready (function () {
 
-$('.create-button').click (function () {
-
-  axios.post ('http://localhost:3000/add', {todo_title: 'PHP', todo_desc: 'Learn PHP', submission_date: '2020-09-14'})
+  let $retrieve = () => {
+    axios.get ('http://localhost:3000/')
     .then (res => {
       console.log (res.data);
+
+      let data = res.data;
+      
+      let $inputGroup = 
+        `<div class="input-group mt-3">
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <input type="checkbox" aria-label="Checkbox for following text input">
+            </div>
+          </div>
+            <input value="" readonly type="text" class="form-control">
+        </div>`
+
+      console.log ($inputGroup);
+
+      $('.todo-list').text (' ');
+
+      data.forEach ((d, i) => {
+        console.log (d);
+        $('.todo-list').append ($inputGroup);
+        // console.log ($($('.todo-list').find ('.form-control')[1]));
+        $($('.todo-list').find ('.form-control')[i]).attr ('value', d.todo_id + ', ' + d.todo_title + ', ' + d.todo_desc);
+      })
+
+      // $('ul').append ('<li>K!</li>').append ('<li>K!</li>').append ('<li>K!</li>')
     })
-});
+  }
 
-$('.update-button').click (function () {
+  $retrieve ();
+  
+  $('.create-button').click (function () {
 
-  axios.put('http://localhost:3000/update', { todo_id: 13, todo_title: 'MySQL', todo_desc: 'Learn MySQL', submission_date: '2020-09-14'})
-    .then (res => {
-      console.log (res.data);
-    })
-});
+    axios.post ('http://localhost:3000/add', {todo_title: 'PHP', todo_desc: 'Learn PHP', submission_date: '2020-09-14'})
+      .then (res => {
+        console.log (res.data);
 
-$('.delete-button').click (function () {
+        $retrieve ();
+      })
+  });
 
-  axios.post ('http://localhost:3000/remove', {todo_id: 13})
-    .then (res => {
-      console.log (res.data);
-    })
+  $('.update-button').click (function () {
+
+    axios.put('http://localhost:3000/update', {todo_id: 16, todo_title: 'CSS', todo_desc: 'Learn CSS', submission_date: '2020-09-16'})
+      .then (res => {
+        console.log (res.data);
+
+        $retrieve ();
+      })
+  });
+
+  $('.delete-button').click (function () {
+
+    axios.post ('http://localhost:3000/remove', {todo_id: 14})
+      .then (res => {
+        console.log (res.data);
+
+        $retrieve ();
+      })
+  });
+
 });
